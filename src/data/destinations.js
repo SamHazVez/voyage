9cfcb4f8f2edc3cd3029bdf0e4fb3f26bec99a0a
SamuelVezeauUlaval <samuel.vezeau.1@ulaval.ca>
2025-08-18 23:15:39 -0400

    feat: added new features: Colisée de Rome, Mont Palatin, Forum Romain, Fontaine de Trevi, Pantheon, Piazza Navona, Musee du Vatican, Chapelle Sixtine, Place Saint-Pierre, Basilique Saint-Pierre, Pompei, Vésuve, Amalfi, Matera, Gravina

--- src/data/destinations.js
+++ src/data/destinations.js
@@ -9,86 +9,82 @@ export const destinations = [
                 popUp: "Thermes de Caracalla",
             },
             {
+                id: "colisee",
                 geocode: [41.8905, 12.4926],
                 popUp: "Colisée de Rome",
             },
             {
+                id: "palatin",
                 geocode: [41.888333, 12.486944],
                 popUp: "Mont Palatin",
             },
             {
+                id: "forum",
                 geocode: [41.89253, 12.48571],
                 popUp: "Forum romain",
             },
             {
+                id: "trevi",
                 geocode: [41.900875, 12.483167],
                 popUp: "Fontaine de Trévi",
             },
             {
+                id: "pantheon",
                 geocode: [41.898604, 12.476816],
                 popUp: "Panthéon",
             },
             {
+                id: "pantheon",
                 geocode: [41.898889, 12.473056],
                 popUp: "Piazza Navona",
             },
             {
+                id: "museevatican",
                 geocode: [41.90639, 12.45444],
                 popUp: "Musées du Vatican",
             },
             {
+                id: "chapellesixtine",
                 geocode: [41.903036, 12.454398],
                 popUp: "Chapelle Sixtine",
             },
             {
+                id: "piazzastpierre",
                 geocode: [41.902222, 12.456389],
                 popUp: "Place Saint-Pierre",
             },
             {
+                id: "basiliquestpierre",
                 geocode: [41.90222, 12.45333],
                 popUp: "Basilique Saint-Pierre",
             },
             {
+                id: "pompei",
                 geocode: [40.751111, 14.49],
                 popUp: "Pompéi",
             },
             {
+                id: "vesuve",
                 geocode: [40.821389, 14.425556],
                 popUp: "Vésuve",
             },
             {
+                id: "amalfi",
                 geocode: [40.633333, 14.6],
                 popUp: "Côte amalfitaine",
             },
             {
+                id: "matera",
                 geocode: [40.666389, 16.610278],
                 popUp: "Sassi di Matera",
             },
             {
+                id: "gravina",
                 geocode: [40.81667, 16.41667],
                 popUp: "Gravina in Puglia",
             },
             {
-                geocode: [41.7, 12.68333],
-                popUp: "Genzano di Roma",
-            },
-        ],
-    },
-    {
-        id: "calgary",
-        date: "Juin 2025",
-        markers: [
-            {
-                geocode: [41.7, 12.68333],
-                popUp: "Genzano di Roma",
-            },
-        ],
-    },
-    {
-        id: "croisiere",
-        date: "Juin 2025",
-        markers: [
-            {
+                id: "genzano",
                 geocode: [41.7, 12.68333],
                 popUp: "Genzano di Roma",
             },
